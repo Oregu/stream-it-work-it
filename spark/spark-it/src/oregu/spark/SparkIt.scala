7354a50eb6b90dca59f010a799ff81ada1c37bf1
Oregu <oleg.burykin@gmail.com>
2015-10-28 18:42:09 -0700

    Fixing Spark non-serializable exception.

--- spark/spark-it/src/oregu/spark/SparkIt.scala
+++ spark/spark-it/src/oregu/spark/SparkIt.scala
@@ -4,9 +4,9 @@ import java.util.Properties
 
 import kafka.serializer.StringDecoder
 import org.apache.kafka.clients.producer._
-import org.apache.kafka.clients.producer.ProducerConfig.{BOOTSTRAP_SERVERS_CONFIG, KEY_SERIALIZER_CLASS_CONFIG, VALUE_SERIALIZER_CLASS_CONFIG}
+import org.apache.kafka.clients.producer.ProducerConfig._
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.dstream.{InputDStream, DStream}
 import org.apache.spark.streaming.kafka._
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
@@ -14,6 +14,22 @@ object SparkIt {
 
   def main(args: Array[String]): Unit = {
 
+    val (ssc, directKafkaStream) = fromKafka
+    val wordCounts = spark(directKafkaStream)
+    toKafka(wordCounts)
+
+    // Start the computation
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+  def spark(directKafkaStream: InputDStream[(String, String)]): DStream[(String, Long)] = {
+    val lines = directKafkaStream.map(_._2)
+    val words = lines.flatMap(_.split(" "))
+    words.map(x => (x, 1L)).reduceByKey(_ + _)
+  }
+
+  def fromKafka: (StreamingContext, InputDStream[(String, String)]) = {
     val kafkaConfigs = Map[String, String](
       BOOTSTRAP_SERVERS_CONFIG      -> "kafka:9092",
       KEY_SERIALIZER_CLASS_CONFIG   -> "org.apache.kafka.common.serialization.StringSerializer",
@@ -26,33 +42,24 @@ object SparkIt {
     val directKafkaStream = KafkaUtils.createDirectStream[
         String, String, StringDecoder, StringDecoder](
         ssc, kafkaConfigs, kafkaTopics)
-
-    val lines = directKafkaStream.map(_._2)
-    val words = lines.flatMap(_.split(" "))
-    val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
-
-    sendToKafka(wordCounts)
-
-    // Start the computation
-    ssc.start()
-    ssc.awaitTermination()
+    (ssc, directKafkaStream)
   }
 
-  def sendToKafka(wordCounts: DStream[(String, Long)]) = {
+  def toKafka(wordCounts: DStream[(String, Long)]) = {
     val configs = new Properties
     configs.put(BOOTSTRAP_SERVERS_CONFIG,      "kafka:9092")
     configs.put(KEY_SERIALIZER_CLASS_CONFIG,   "org.apache.kafka.common.serialization.StringSerializer")
     configs.put(VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
 
-    val producer = new KafkaProducer[Nothing, String](configs)
 
     val topic = "noise-extract"
     wordCounts.foreachRDD { rdd =>
       rdd.foreach { count =>
+        val producer = new KafkaProducer[Nothing, String](configs)
         val rec = new ProducerRecord(topic, count._2.toString)
         producer.send(rec)
+        producer.close()
       }
     }
-    producer.close()
   }
 }
